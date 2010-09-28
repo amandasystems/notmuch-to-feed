4600b4da7a80c39cbe7bf907f56a5708218dc616
krille <krille@mamma.(none)>
2010-09-28 18:48:12 +0200

    added config file, removed week-old restriction

--- notmuch-to-feed.py
+++ notmuch-to-feed.py
@@ -9,12 +9,13 @@
 # mail tagged with "rek" in notmuch.
 
 import commands as cm
+import os
 import json
 import datetime
 import PyRSS2Gen
 import sys
 import time
-
+import ConfigParser
 
 def strip_mail_crap(mail):
     "skip some uninteresting data"
@@ -23,57 +24,6 @@ def strip_mail_crap(mail):
 def body(mail):
     "return the body of the mail"
     return strip_mail_crap(mail)['body']
-
-# def content_clean(mail):
-#     "Trim and strip away feed2imap footer and notmuch URL index. Hopefully, don't touch other emails."
-#     def find_start(str):
-#         inurl=False
-#         foundurl=False
-#         start = 0
-#         for a in s:
-#             if a == '<' and not inurl and not foundurl:
-#                 inurl=True
-#                 start += 1
-#             elif a == '>':
-#                 inurl=False
-#                 foundurl=True
-#                 start += 1
-#             elif a == '\n':
-#                 if inurl: # URLs do not span multiple lines!
-#                     error("parse error")
-#                     break
-#                 elif foundurl: 
-#                     return start + 1 # We've found our URL and now a \n -- we're home.
-#             else: 
-#                 start += 1
-#         return start
-
-#     def find_end(str):
-#         hyphens_found=0 # we want to find two in a row and then a newline
-#         end=0
-#         for a in str:
-#             if a == '\n' and hyphens_found==2:
-#                 return end # skip both hyphens and newlines
-#             if a == ' ' and hyphens_found==2: #jump trailing ' '
-#                 end += 1
-#             elif a == '-':
-#                 hyphens_found += 1
-#             else:
-#                 hyphens_found = 0
-#                 end += 1
-#         print end
-#         return end
-
-#     def strip_url_index(s):
-#         return s[:s.find("] \nhttp://")-2] #
-
-#     s = content(mail)
-#     begin = find_start(mail)
-#     end = find_end(s)
-#     if end == 0:
-#         end = len(s)
-    
-#     return strip_url_index(s[begin:end]).lstrip().rstrip()
 
 def content(mail):
     "return raw, unformatted mail content"
@@ -109,20 +59,24 @@ def html(mail):
     return cm.getstatusoutput("notmuch part --part=" +
                               str(find_html_part(mail)) + " id:" +
                               notmuch_id(mail))[1]
+
 def strip_feed2imap_table(html):
     return html #FIXME
 
-end_time=time.time() # now
-start_time=time.time() - (7 * 24 * 60 * 60) # 1 w earlier
-# this will output all mails matching tag "rek" recieved the last week:
-notmuch_out = cm.getstatusoutput('notmuch show --format=json tag:rek and ' + str(start_time) + '..' + str(end_time)) 
+config = ConfigParser.ConfigParser()
+conf_file = os.path.expanduser('~/.notmuch-to-feedrc')
+if os.path.exists(conf_file):
+    config.readfp(open(conf_file))
+
+# get all with the tag, filter later. until notmuch has this feature built in..
+notmuch_out = cm.getstatusoutput('notmuch show --format=json tag:' + config.get("entries", "tag"))
 status=notmuch_out[0] # exit status
 message=notmuch_out[1] # actual JSON output
 
 if(status != 0):
     exit("error from notmuch, exit status" + str(status) + "message: " + message)
 
-mails = json.loads(message)
+mails = json.loads(message)[:config.getint("entries", "number")]
 
 def gen_item (mail):
     return PyRSS2Gen.RSSItem(
@@ -132,13 +86,10 @@ def gen_item (mail):
         guid = PyRSS2Gen.Guid(find_url(content(mail))),
         pubDate = datetime.datetime.utcfromtimestamp(timestamp(mail)))
 
-#print map(html, mails)
-
-
 rss = PyRSS2Gen.RSS2(
-    title = "Albins rekommenderatflöde",
-    link = "http://eval.nu/rekommenderat.rss",
-    description = "Ett flöde med rekommenderad läsning",
+    title         = config.get("feed", "title"),
+    link          = config.get("feed", "link"),
+    description   = config.get("feed", "description"),
     lastBuildDate = datetime.datetime.utcnow(),
     items = map(gen_item, mails))
 
