fa69c07cc6ba177b68a0ac79433cac6d6da68163
Albin Stjerna <albin@eval.nu>
2010-12-29 12:21:42 +0100

    Use notmuch bindings instead, they're so much faster it's silly.

--- notmuch-get-filenames.py
+++ notmuch-get-filenames.py
@@ -10,27 +10,14 @@
 # for cleaning up and archiving mail.
 # usage: notmuch-get-filenames <query>
 
-import json
 import sys
-import commands as cm
+import notmuch
 import string
-
-def parse_filename(js_mail):
-    "returns the filename string from a parsed json mail entry"
-    return js_mail[0][0]['filename']
-
 
 query=string.join(sys.argv[1:], sep=' ') # get all arguments, except
 # the program name, and concatenate them into a string separated by
 # space, that is, return the entire argument list as it was entered.
 
-notmuch_out = cm.getstatusoutput('notmuch show --format=json %s' % query)
-status=notmuch_out[0] # exit status
-message=notmuch_out[1] # actual JSON output
-
-if(status != 0):
-    exit("error from notmuch, exit status" + str(status) + "message: " + message)
-    
-mails = json.loads(message)
-for mail in mails:
-    print parse_filename(mail)
+q = notmuch.Database().create_query(query)
+for m in q.search_messages():
+    print m.get_filename()
